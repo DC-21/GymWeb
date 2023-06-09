3470862f5be95fee9430455b1476f1f8cc9215d0
DC-21 <cholahkuboko@gmail.com>
2023-06-09 11:12:35 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -42,11 +42,11 @@ export default function App() {
     <section className='h-screen w-full pt-16' style={{ backgroundColor: '#0a0911' }}>
       <div className='flex'>
         <div className='w-1/2 items-center px-10 relative'>
-          <p className='text-white font-bold text-4xl text-left pt-20 justify-center px-12'>Transform Your
+          <p className='text-white font-bold text-4xl text-left pt-20 justify-center px-12'>Transform Your<br/>
           Fitness And Build Your Life</p>
         </div>
         <div className='w-1/2 items-center'>
-          <img  className='items-center px-6  mt-10' src={fit}/>
+          <img  className='items-center px-6  mt-10 rounded-sm' src={fit}/>
         </div>
       </div>
     </section>
